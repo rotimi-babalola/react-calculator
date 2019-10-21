cd2196b04f4eadf88d9db7b08c12261dde765a5b
Rotimi Babalola <rotimi.babalola@andela.com>
2019-10-21 09:20:15 +0100

    Create initial button grid

---
+++ src/components/CalculatorGrid.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import '../scss/calculator-grid.scss';
+
+function CalculatorGrid() {
+  return (
+    <div className="c-calculator-grid">
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>0</button>
+    </div>
+  );
+}
+
+export default CalculatorGrid;
