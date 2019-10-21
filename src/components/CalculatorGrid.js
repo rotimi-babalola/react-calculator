9b8cd845dc0e44f08aff558415ccaa78d8229972
Rotimi Babalola <rotimi.babalola@andela.com>
2019-10-21 15:41:55 +0100

    Add text box for number entry

--- src/components/CalculatorGrid.js
+++ src/components/CalculatorGrid.js
@@ -4,17 +4,20 @@ import '../scss/calculator-grid.scss';
 
 function CalculatorGrid() {
   return (
-    <div className="c-calculator-grid">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>0</button>
+    <div className="c-calculator">
+      <input type="number" name="calculator-input" id="input" />
+      <div className="c-calculator__button-grid">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>0</button>
+      </div>
     </div>
   );
 }
