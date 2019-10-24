0dac8033cffa9a71114800b3b15fe9bdd7b1e19a
Rotimi Babalola <rotimi.babalola@andela.com>
2019-10-24 19:31:22 +0100

    Perform plus and minus operations

--- src/components/CalculatorGrid.js
+++ src/components/CalculatorGrid.js
@@ -1,24 +1,35 @@
 import React, { useState } from 'react';
+import { evaluate } from 'mathjs';
 
 import '../scss/calculator-grid.scss';
 
 function CalculatorGrid() {
-  const numbers = [...Array(10).keys()];
-  const [number, setNumber] = useState('');
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  const [inputValue, setInputValue] = useState('');
 
   const handleClick = el => {
-    const str = `${number}${el}`;
-    setNumber(Number(str));
+    const str = `${inputValue}${el}`;
+    setInputValue(str);
+  };
+
+  const getResult = () => {
+    try {
+      const result = evaluate(inputValue);
+      setInputValue(result);
+    } catch (error) {
+      console.log(error);
+      alert('Invalid input');
+    }
   };
 
   return (
     <div className="c-calculator">
       <input
-        type="number"
+        type="text"
         name="calculator-input"
         id="input"
-        value={number || 0}
-        onChange={evt => setNumber(evt.target.value)}
+        value={inputValue || 0}
+        onChange={evt => setInputValue(evt.target.value)}
       />
       <div className="c-calculator__button-grid">
         {numbers.map(el => (
@@ -26,8 +37,18 @@ function CalculatorGrid() {
             {el}
           </button>
         ))}
-        <button>+</button>
-        <button>-</button>
+        <button onClick={() => handleClick('+')}>+</button>
+        <button onClick={() => handleClick('-')}>-</button>
+        <button onClick={() => setInputValue('')}>C</button>
+        <button
+          className="c-calculator__button-grid--equal"
+          onClick={() => getResult()}
+        >
+          =
+        </button>
+        <button onClick={() => setInputValue(inputValue.slice(0, -1))}>
+          ⬅
+        </button>
       </div>
     </div>
   );
